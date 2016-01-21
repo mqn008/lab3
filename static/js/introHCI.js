cb6c3ff2f248a723e0b9da69668e833ad9cf00c3
Michelle Nguyen <mqn008@ucsd.edu>
2016-01-21 22:15:57 +0000

    javascript lab: I am sorry this is late, class just ended and I couldn't connect to the internet.

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$('h4').css("text-decoration","underline");
 })
 
 /*
@@ -11,8 +12,41 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("Clicked!");
+		$('.jumbotron p').addClass("active");
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$('#submitBtn').click(function(e) {
+		var projVal = $('#project').val();
+		var projWidth = $('#width').val();
+		var projText = $('#description').val();
+		var projDesc = $(projVal).find('.project-description');
+		$(projVal).animate({
+			width: projWidth
+		});
+		$(projDesc).text(projText);
+	});
+
+	$('h4').click(function(e) {
+		$(this).css({"font-family":"georgia","font-style":"italic"});
+	});
+
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	console.log("Project clicked");
+    var projectTitle = $(this).find("p").text();
+    console.log(projectTitle);
+    var jumbotronHeader = $('.jumbotron h1');
+    $(jumbotronHeader).text(projectTitle);
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+		if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		$(containingProject).fadeOut();
+	}
 }
